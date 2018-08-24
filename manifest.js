8df7c3ae5625d327cb9acc5abddb43f38ffcaffb
XBjornsen <xaviertr7@gmail.com>
2018-08-24 15:54:43 +0930

    adding stuff
    nothing

---
+++ manifest.js
@@ -0,0 +1,20 @@
+{
+    name: "Charge4lyfe",
+    short_name: "Charge",
+    icons: [
+        {
+            src: "images/icons/icon-128.png",
+            sizes: "128x128",
+            type: "image/png"
+        },
+        {
+            src: "images/icons/icon-512.png",
+            sizes: "512x512",
+            type: "image/png"
+        }
+    ],
+    start_url: ".",
+    display: "standalone",
+    background_color: "#FFF",
+    theme_color: "#123557"
+}
